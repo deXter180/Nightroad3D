a7c6840ef08012f2a578b3ad6a8df647ca262c76
deXter180 <paritosh.mishra21@gmail.com>
2020-10-28 18:07:43 +0530

    Build 01a
    Enemy Categories  & Properties added
    Health System fixed

---
+++ Assets/Scripts/Enemy/EnemyManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+public abstract class EnemyManager : IEnemyStat
+{
+    public abstract int id { get; set; }
+    public abstract string name { get; set; } 
+}
+
+public enum EnemyTypes
+{
+    Giant,
+    Fighter
+}
---
+++ Assets/Scripts/Enemy/IEnemy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+
+public interface IEnemyStat
+{
+    int id { get; set; }
+    string name { get; set; }
+}
+public interface IDamage
+{
+    void DoDamage(int dmg);
+}
