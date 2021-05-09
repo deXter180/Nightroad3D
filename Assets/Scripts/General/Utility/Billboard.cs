deb086d93183eecaa3174cc43c902fb52d728fd1
Noble_Alfred <dedicateddark@gmail.com>
2021-05-09 22:58:21 +0530

    Basic asset implementation v0

---
+++ Assets/Scripts/General/Utility/Billboard.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.forward = new Vector3(Camera.main.transform.forward.x, transform.forward.y, Camera.main.transform.forward.z);
+    }
+}
